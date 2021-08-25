0a9086e6305a5bc41ee084d8a32ec985d7613ecf
mohamedSabry0 <mohamedsabry3023@gmail.com>
2021-08-25 16:55:32 +0200

    add basic html structure and JS functions

--- src/index.js
+++ src/index.js
@@ -1,14 +1,45 @@
 import _ from "lodash";
 import "./style.css";
 
-function component() {
-  const element = document.createElement("div");
+const mockList = [
+  {
+    description: "task 1",
+    completed: true,
+    index: 1,
+  },
+  {
+    description: "task 2",
+    completed: false,
+    index: 2,
+  },
+  {
+    description: "task 3",
+    completed: false,
+    index: 3,
+  },
+];
+const createTask = (task) => {
+  const taskText = document.createElement("p");
+  taskText.classList.add("m-0");
+  taskText.textContent = task.description;
+  const completedCheck = document.createElement("input");
+  completedCheck.type = "checkbox";
+  const dragBtn = document.createElement("div");
+  dragBtn.classList.add("drag-btn");
+  console.log(dragBtn);
+  return [taskText, completedCheck, dragBtn];
+};
 
-  // Lodash, now imported
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
+const populateList = (list) => {
+  const ul = document.querySelector(".tasks-container");
+  ul.classList.add("list-unstyled");
+  list.forEach((task) => {
+    const li = document.createElement("li");
 
-  return element;
-}
+    createTask(task).forEach((el) => li.appendChild(el));
+    li.className = "d-flex";
+    ul.appendChild(li);
+  });
+};
 
-document.body.appendChild(component());
-error;
+populateList(mockList);

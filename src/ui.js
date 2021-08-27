17c686c171527c1b3ad8c3d47ac4fdb4bf1108c4
mohamedSabry0 <mohamedsabry3023@gmail.com>
2021-08-27 22:45:53 +0200

    edit style on action

--- src/ui.js
+++ src/ui.js
@@ -1,25 +1,12 @@
 import sortByIndex from './helpers.js';
 import changeStatus from './complete_action.js';
+import storage from './store.js';
 
 const UI = (() => {
-  const createTask = (task) => {
-    const taskText = document.createElement('p');
-    taskText.classList.add('m-0', 'px-2');
-    taskText.textContent = task.description;
-    const completedCheck = document.createElement('input');
-    completedCheck.type = 'checkbox';
-    completedCheck.checked = task.completed;
-    completedCheck.className = 'completed-input';
-    completedCheck.addEventListener('change', () => {
-      changeStatus(task);
-    });
-    const dragBtn = document.createElement('div');
-    dragBtn.classList.add('drag-btn', 'ms-auto', 'me-2');
-    return [completedCheck, taskText, dragBtn];
-  };
-
+  let createTask;
   const populateList = (list) => {
     const ul = document.querySelector('.tasks-container');
+    ul.innerHTML = '';
     list.sort(sortByIndex);
     list.forEach((task) => {
       const li = document.createElement('li');
@@ -28,6 +15,22 @@ const UI = (() => {
       li.className = 'd-flex p-2 border-bottom';
       ul.appendChild(li);
     });
+  };
+  createTask = (task) => {
+    const taskText = document.createElement('p');
+    taskText.textContent = task.description;
+    const completedCheck = document.createElement('input');
+    completedCheck.type = 'checkbox';
+    completedCheck.checked = task.completed;
+    taskText.className = task.completed ? 'm-0 px-2 completed' : 'm-0 px-2';
+    completedCheck.className = 'completed-input';
+    completedCheck.addEventListener('change', () => {
+      changeStatus(task);
+      populateList(storage.get());
+    });
+    const dragBtn = document.createElement('div');
+    dragBtn.classList.add('drag-btn', 'ms-auto', 'me-2');
+    return [completedCheck, taskText, dragBtn];
   };
 
   return {

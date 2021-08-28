917f1dc6714e96c72eb5e7b52368d7ae5dd4300d
mohamedSabry0 <mohamedsabry3023@gmail.com>
2021-08-28 23:46:45 +0200

    fix index issue

--- src/remove_all_completed.js
+++ src/remove_all_completed.js
@@ -1,8 +1,18 @@
 import storage from './store.js';
 
+const reArrangeIndex = (list) => {
+  let index = 1;
+  list.forEach((element) => {
+    element.index = index;
+    index += 1;
+  });
+  return list;
+};
+
 const clearAllCompleted = () => {
   let list = storage.get();
   list = list.filter((task) => !task.completed);
+  list = reArrangeIndex(list);
   storage.setList(list);
 };
 
